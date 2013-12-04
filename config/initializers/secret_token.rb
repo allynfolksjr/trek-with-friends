3d9710d6c748f33eeeacf9ce87ebf856eae38351
Nikky Southerland <nikky@uw.edu>
2013-12-03 21:33:02 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrekWithFriends::Application.config.secret_key_base = 'a8c7649192bad600f702e529f1a6591730f9e8db51f538e54e2fded93c3969944e1e5e30778dfe850b01bdea7d33656b420bf34f1237de50b1009e6eee854d33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrekWithFriends::Application.config.session_store :cookie_store, key: '_trek-with-friends_session'
